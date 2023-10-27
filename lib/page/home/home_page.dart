8b548bdb803e424389cf2c6a5e349fea5615170b
haidar ahlan g <ahlangaffar@gmail.com>
2023-10-27 13:22:31 +0700

    set up home

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    PreferredSizeWidget header(){
-      return AppBar(
-        elevation: 0,
-        title: const Text('Home page'),
-      );
-    }
     return Scaffold(
-      appBar: header(),
-      body: Center(child: Text('Ini Home page'),)
-    );
+        body: Center(
+      child: Text('Ini Home page'),
+    ));
   }
 }
